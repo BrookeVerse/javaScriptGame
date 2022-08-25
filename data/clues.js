31ed8679f10a2f36929bcfc79880acd592a31c3a
BrookVerse <brookewaldorf90@gmail.com>
2022-08-25 12:54:08 +0100

    Setting up functions for clues

---
+++ data/clues.js
@@ -0,0 +1,9 @@
+export const cluesArray = [
+  {
+    title: "Clue One",
+    clue: "I am a home of knowledge, both banal and profound. In grand halls and small homes I can be found. I am a home for things of many leaves, but my many residents are not living trees. What am I?",
+    number: 4,
+  },
+];
+
+export default cluesArray;
---
+++ game.js
@@ -0,0 +1,23 @@
+import { cluesArray } from "./data/clues.js";
+//Need to grab the button
+const clueOne = document.querySelector(".game__clueOne");
+const container = document.querySelector(".game");
+//function for content to change
+
+const openClue = (clue) => {
+  return `<div class="clueContainer">
+    <div class="clue"> 
+        <h3 class="clue__heading">${clue.title}</h3>
+        <p class="clue__info">${clue.clue}</p>
+        <p class="clue__number">${clue.number}</p> 
+    </div>
+</div>`;
+};
+
+//Function to collect data from the object array
+cluesArray.forEach((clue) => {
+  clueOne.addEventListener("click", () => {
+    container.innerHTML = "";
+    container.innerHTML = openClue(clue)[0];
+  });
+});
