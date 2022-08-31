46305d52c4690c03974993859b52962062427c2a
BrookVerse <brookewaldorf90@gmail.com>
2022-08-31 15:51:29 +0100

    refactored functions

--- game.js
+++ game.js
@@ -1,90 +1,132 @@
 import { cluesArray } from "./data/clues.js";
-//Need to grab the button
+//Grabbing DOM elements to use for functions
 const clueOne = document.querySelector(".game__clueOne");
 const clueTwo = document.querySelector(".game__clueTwo");
 const clueThree = document.querySelector(".game__clueThree");
 const clueFour = document.querySelector(".game__clueFour");
 const container = document.querySelector(".game");
-const refreshPress = document.querySelector(".game__marty");
+const martyRefresh = document.querySelector(".game__marty");
 const martyContainer = document.querySelector(".game__martyContainer");
 const inputBox = document.querySelector(".input__code");
 const inputContainer = document.querySelector(".input");
 const gameboard = document.querySelector(".game__img");
 const gameHeading = document.querySelector(".home__title");
-//Function to collect data from the object array
+
+//This is a global array to count User clicks
+let click = 0;
+
+//This listens for when the user clicks and sets of the clickCounter function
+gameboard.addEventListener("click", () => {
+  clickCounter();
+});
+
+//This listens for when the first clue is clicked and changes DOM elements
 cluesArray.forEach(() => {
   clueOne.addEventListener("click", () => {
-    container.innerHTML = "";
-    inputContainer.innerHTML = "";
+    removeContent();
     const clue = cluesArray[1];
     container.innerHTML = openClue(clue);
   });
 });
 
-//eventlisterner to call clue two
+//This listens for when the second clue is clicked and changes Dom elements
 cluesArray.forEach(() => {
   clueTwo.addEventListener("click", () => {
-    container.innerHTML = "";
-    inputContainer.innerHTML = "";
+    removeContent();
     const clue = cluesArray[2];
     container.innerHTML = openClue(clue);
   });
 });
 
-//event listener for clue three
+//This listens for when the third clue is clicked and changes DOM elements
 cluesArray.forEach(() => {
   clueThree.addEventListener("click", () => {
-    container.innerHTML = "";
-    inputContainer.innerHTML = "";
+    removeContent();
     const clue = cluesArray[3];
     container.innerHTML = openClue(clue);
   });
 });
 
-//event listener for clue four
+//This listens for when the fourth clue is clicked and changes DOM elements
 cluesArray.forEach(() => {
   clueFour.addEventListener("click", () => {
-    container.innerHTML = "";
-    inputContainer.innerHTML = "";
+    removeContent();
     const clue = cluesArray[4];
     container.innerHTML = openClue(clue);
   });
 });
 
-//event listener for when the page needs refreshing
-refreshPress.addEventListener("click", () => {
+//This listens for when the user types numbers into the input box
+inputBox.addEventListener("input", () => {
+  userInput();
+});
+
+//This listens for when the Marty img is pressed to refresh the page
+martyRefresh.addEventListener("click", () => {
   window.location.reload();
 });
 
-//event listener for when the answer is put into the input box
-inputBox.addEventListener("input", () => {
+//This function is for counting how many times the user clicks and sends them to the Game Over Screen
+const clickCounter = () => {
+  click += 1;
+  if (click === 3) {
+    wrongClick();
+  }
+};
+
+//This function is to erase container and inputContainer HTML
+const removeContent = () => {
+  container.innerHTML = "";
+  inputContainer.innerHTML = "";
+};
+
+//This function will call on removeContent and gameOverScreen
+const wrongClick = () => {
+  removeContent();
+  gameOverScreen();
+};
+
+//This function will change the HTMl to create a Game Over Screen
+const gameOverScreen = () => {
+  martyContainer.innerHTML = "";
+  gameHeading.innerHTML = "You Lost! Try Again.";
+  gameHeading.style.color = "#ff6961";
+  container.innerHTML += openHattie();
+};
+
+//This function will see in the input code is correct and call on contentRemove and youWon
+const userInput = () => {
   const inputAnswer = 26789;
   if (inputBox.value == inputAnswer) {
-    inputContainer.innerHTML = "";
-    container.innerHTML = "";
-    martyContainer.innerHTML = "";
-    gameHeading.innerHTML = "You Won! Marty is Free!";
-    gameHeading.style.color = "#E7D27C";
-    container.innerHTML += winnerScreen();
-    inputContainer.innerHTML += exitDoor();
+    removeContent();
+    youWon();
   }
-});
+};
 
-//function is adding a winner screen
+//This function will change the HTML to create a You Won Screen
+const youWon = () => {
+  martyContainer.innerHTML = "";
+  gameHeading.innerHTML = "You Won! Marty is Free!";
+  gameHeading.style.color = "#E7D27C";
+  container.innerHTML += winnerScreen();
+  inputContainer.innerHTML += exitDoor();
+};
+
+//This function will change DOM HTML that creates the You Won Screen
 const winnerScreen = () => {
   return `<div class= "winnerScreen">
   <img src="./assets/happyMarty.png" alt="Marty Marshmallow" class="game__happyMarty" />
   </div>`;
 };
 
-//function that add html to make a button that takes you back to the home screen
+//This function will change DOM HTML to create a link button to exit the game after you win
 const exitDoor = () => {
   return `<div class= "exit">
   <a href="./index.html"><button class="home__refresh">Exit Here!</button></a>
   </div>`;
 };
 
-//function for content to change
+//This function will change DOM HTML to create the clue Cards using the clueArray Objects
 const openClue = (clue) => {
   return `<div class="clueContainer">
     <div class="clue"> 
@@ -95,25 +137,7 @@ const openClue = (clue) => {
 </div>`;
 };
 
-//function that will have three clicks and cause something
-//Move this to functions area.
-const wrongClick = () => {
-  inputContainer.innerHTML = "";
-  container.innerHTML = "";
-  martyContainer.innerHTML = "";
-  gameHeading.innerHTML = "You Lost! Try Again.";
-  gameHeading.style.color = "#ff6961";
-  container.innerHTML += openHattie();
-};
-
-let click = 0;
-gameboard.addEventListener("click", () => {
-  click += 1;
-  if (click === 3) {
-    wrongClick();
-  }
-});
-
+//This function will change DOM HTML to create the Game Over Screen
 const openHattie = () => {
   return `<div class="hattieContainer">
   <img src="./assets/hattieOver.png" alt="Hattie Hot-Choclate" class="game__hattie" />
